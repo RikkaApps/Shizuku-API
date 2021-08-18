5b30dee8cb4919dbd37efec2400324f45215d4b9
RikkaW <rikka@shizuku.moe>
2021-08-18 11:10:33 +0800

    Update Logger class

--- server-shared/src/main/java/rikka/shizuku/server/util/Logger.java
+++ server-shared/src/main/java/rikka/shizuku/server/util/Logger.java
@@ -2,14 +2,31 @@ package rikka.shizuku.server.util;
 
 import android.util.Log;
 
+import java.io.IOException;
 import java.util.Locale;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
 
 public class Logger {
 
-    private final String TAG;
+    private final String tag;
+    private final java.util.logging.Logger LOGGER;
 
-    public Logger(String TAG) {
-        this.TAG = TAG;
+    public Logger(String tag) {
+        this.tag = tag;
+        this.LOGGER = null;
+    }
+
+    public Logger(String tag, String file) {
+        this.tag = tag;
+        this.LOGGER = java.util.logging.Logger.getLogger(tag);
+        try {
+            FileHandler fh = new FileHandler(file);
+            fh.setFormatter(new SimpleFormatter());
+            LOGGER.addHandler(fh);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean isLoggable(String tag, int level) {
@@ -17,104 +34,111 @@ public class Logger {
     }
 
     public void v(String msg) {
-        if (isLoggable(TAG, Log.VERBOSE)) {
-            Log.v(TAG, msg);
+        if (isLoggable(tag, Log.VERBOSE)) {
+            println(Log.VERBOSE, msg);
         }
     }
 
     public void v(String fmt, Object... args) {
-        if (isLoggable(TAG, Log.VERBOSE)) {
-            Log.v(TAG, String.format(Locale.ENGLISH, fmt, args));
+        if (isLoggable(tag, Log.VERBOSE)) {
+            println(Log.VERBOSE, String.format(Locale.ENGLISH, fmt, args));
         }
     }
 
     public void v(String msg, Throwable tr) {
-        if (isLoggable(TAG, Log.VERBOSE)) {
-            Log.v(TAG, msg, tr);
+        if (isLoggable(tag, Log.VERBOSE)) {
+            println(Log.VERBOSE, msg + '\n' + Log.getStackTraceString(tr));
         }
     }
 
     public void d(String msg) {
-        if (isLoggable(TAG, Log.DEBUG)) {
-            Log.d(TAG, msg);
+        if (isLoggable(tag, Log.DEBUG)) {
+            println(Log.DEBUG, msg);
         }
     }
 
     public void d(String fmt, Object... args) {
-        if (isLoggable(TAG, Log.DEBUG)) {
-            Log.d(TAG, String.format(Locale.ENGLISH, fmt, args));
+        if (isLoggable(tag, Log.DEBUG)) {
+            println(Log.DEBUG, String.format(Locale.ENGLISH, fmt, args));
         }
     }
 
     public void d(String msg, Throwable tr) {
-        if (isLoggable(TAG, Log.DEBUG)) {
-            Log.d(TAG, msg, tr);
+        if (isLoggable(tag, Log.DEBUG)) {
+            println(Log.DEBUG, msg + '\n' + Log.getStackTraceString(tr));
         }
     }
 
     public void i(String msg) {
-        if (isLoggable(TAG, Log.INFO)) {
-            Log.i(TAG, msg);
+        if (isLoggable(tag, Log.INFO)) {
+            println(Log.INFO, msg);
         }
     }
 
     public void i(String fmt, Object... args) {
-        if (isLoggable(TAG, Log.INFO)) {
-            Log.i(TAG, String.format(Locale.ENGLISH, fmt, args));
+        if (isLoggable(tag, Log.INFO)) {
+            println(Log.INFO, String.format(Locale.ENGLISH, fmt, args));
         }
     }
 
     public void i(String msg, Throwable tr) {
-        if (isLoggable(TAG, Log.INFO)) {
-            Log.i(TAG, msg, tr);
+        if (isLoggable(tag, Log.INFO)) {
+            println(Log.INFO, msg + '\n' + Log.getStackTraceString(tr));
         }
     }
 
     public void w(String msg) {
-        if (isLoggable(TAG, Log.WARN)) {
-            Log.w(TAG, msg);
+        if (isLoggable(tag, Log.WARN)) {
+            println(Log.WARN, msg);
         }
     }
 
     public void w(String fmt, Object... args) {
-        if (isLoggable(TAG, Log.WARN)) {
-            Log.w(TAG, String.format(Locale.ENGLISH, fmt, args));
+        if (isLoggable(tag, Log.WARN)) {
+            println(Log.WARN, String.format(Locale.ENGLISH, fmt, args));
         }
     }
 
     public void w(Throwable tr, String fmt, Object... args) {
-        if (isLoggable(TAG, Log.WARN)) {
-            Log.w(TAG, String.format(Locale.ENGLISH, fmt, args), tr);
+        if (isLoggable(tag, Log.WARN)) {
+            println(Log.WARN, String.format(Locale.ENGLISH, fmt, args) + '\n' + Log.getStackTraceString(tr));
         }
     }
 
     public void w(String msg, Throwable tr) {
-        if (isLoggable(TAG, Log.WARN)) {
-            Log.w(TAG, msg, tr);
+        if (isLoggable(tag, Log.WARN)) {
+            println(Log.WARN, msg + '\n' + Log.getStackTraceString(tr));
         }
     }
 
     public void e(String msg) {
-        if (isLoggable(TAG, Log.ERROR)) {
-            Log.e(TAG, msg);
+        if (isLoggable(tag, Log.ERROR)) {
+            println(Log.ERROR, msg);
         }
     }
 
     public void e(String fmt, Object... args) {
-        if (isLoggable(TAG, Log.ERROR)) {
-            Log.e(TAG, String.format(Locale.ENGLISH, fmt, args));
+        if (isLoggable(tag, Log.ERROR)) {
+            println(Log.ERROR, String.format(Locale.ENGLISH, fmt, args));
         }
     }
 
     public void e(String msg, Throwable tr) {
-        if (isLoggable(TAG, Log.ERROR)) {
-            Log.e(TAG, msg, tr);
+        if (isLoggable(tag, Log.ERROR)) {
+            println(Log.ERROR, msg + '\n' + Log.getStackTraceString(tr));
         }
     }
 
     public void e(Throwable tr, String fmt, Object... args) {
-        if (isLoggable(TAG, Log.ERROR)) {
-            Log.e(TAG, String.format(Locale.ENGLISH, fmt, args), tr);
+        if (isLoggable(tag, Log.ERROR)) {
+            println(Log.ERROR, String.format(Locale.ENGLISH, fmt, args) + '\n' + Log.getStackTraceString(tr));
         }
+    }
+
+    public int println(int priority, String msg) {
+        if (LOGGER != null) {
+            LOGGER.info(msg);
+        }
+        return Log.println(priority, tag, msg);
     }
 }
