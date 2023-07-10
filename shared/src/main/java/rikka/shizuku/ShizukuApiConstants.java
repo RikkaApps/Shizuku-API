9b501453b1370016ae5b84c772f89644a4012ac2
RikkaW <rikka@shizuku.moe>
2023-07-10 11:15:56 +0800

    Add SERVER_PATCH_VERSION

--- shared/src/main/java/rikka/shizuku/ShizukuApiConstants.java
+++ shared/src/main/java/rikka/shizuku/ShizukuApiConstants.java
@@ -7,6 +7,7 @@ import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP_PREFIX;
 public class ShizukuApiConstants {
 
     public static final int SERVER_VERSION = 13;
+    public static final int SERVER_PATCH_VERSION = 4;
 
     // binder
     public static final String BINDER_DESCRIPTOR = "moe.shizuku.server.IShizukuService";
